f8f7ab70e4ad5be104ea29de36a1ad524e64b5ff
Ruturaj2003 <ruturajhb2003@gmail.com>
2023-12-26 18:14:58 +0530

    Complete short circuit

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -3,6 +3,8 @@ const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -12,14 +14,41 @@ const MultipleReturnsFetchData = () => {
         setUser(data);
       } catch (error) {
         console.log('Well GG WP');
+        setIsError(true);
       }
+      setIsLoading(false);
     };
     fetchUser();
     console.log(user);
   }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <h3>Magic is Happening!!.</h3>
+        <h4>Please Wait A Moment</h4>
+      </>
+    );
+  }
+  if (isError) {
+    return <h3>Something went Wrong</h3>;
+  }
+
   return (
     <>
       <h2>Fetch Data </h2>
+      <h3>{user.login}</h3>
+      <img
+        style={{ widht: '150px', borderRadius: '15px' }}
+        src={user.avatar_url}
+        alt=""
+      />
+      <h4>
+        <a href={user.html_url}>@Follow</a>
+      </h4>
+      <h4>{user.bio}</h4>
+      <h5>{user.company}</h5>
+      <h6>{user.location}</h6>
     </>
   );
 };
--- src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -8,7 +8,17 @@ const ShortCircuitExamples = () => {
   const [user, setUser] = useState({ name: 'john' });
   const [isEditing, setIsEditing] = useState(false);
 
-  return <h2>short circuit - examples</h2>;
+  return (
+    <>
+      <h2>short circuit - examples</h2>
+      <h3>{text || 'Default text'}</h3>
+      <div>{name && <DummyComponent name={user.name}></DummyComponent>}</div>
+    </>
+  );
+};
+
+const DummyComponent = ({ name }) => {
+  return <h4>my name is {name}</h4>;
 };
 
 export default ShortCircuitExamples;
