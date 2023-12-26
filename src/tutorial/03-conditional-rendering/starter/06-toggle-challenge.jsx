6c9109bde5396e673bf3d59330330cdad74fa2c4
Ruturaj2003 <ruturajhb2003@gmail.com>
2023-12-26 18:51:46 +0530

    complete toggle challenge

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [isTrue, setIsTrue] = useState(false);
+
+  const handleClick = () => {
+    setIsTrue(!isTrue);
+  };
+  return (
+    <>
+      <h2>toggle challenge</h2>
+      <button onClick={handleClick}>Click to Add Text</button>
+      <h4>{isTrue && 'You clicked'}</h4>
+    </>
+  );
 };
 
 export default ToggleChallenge;
