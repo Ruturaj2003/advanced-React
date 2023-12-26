81dafcb062d0c704b8d14d4f5d384d3f98f0e19f
Ruturaj2003 <ruturajhb2003@gmail.com>
2023-12-26 19:02:43 +0530

    Complete User Challenge

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react';
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+  function logIn() {
+    setUser({ name: 'RAY' });
+    console.log(user);
+  }
+  function logOut() {
+    setUser(null);
+    console.log(user);
+  }
+  return (
+    <>
+      <h2>user challenge</h2>
+      {user?.name ? (
+        <>
+          <h4>Hello {user.name}</h4>
+          <button onClick={logOut} className="btn">
+            Log Out
+          </button>
+        </>
+      ) : (
+        <>
+          <h4>Please Log IN</h4>
+          <button onClick={logIn} className="btn">
+            Log In
+          </button>
+        </>
+      )}
+    </>
+  );
 };
 
 export default UserChallenge;
