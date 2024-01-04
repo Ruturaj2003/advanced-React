9783c96ae3347db843cc40a21855fcfe23b98480
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-04 19:55:34 +0530

    Finish Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/06-forms/starter/01-controlled-inputs';
+import Starter from './tutorial/06-forms/starter/02-user-challenge';
 import Final from './tutorial/01-useState/final/01-error-example';
 
 function App() {
--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,58 @@
+import { useState } from 'react';
+import { data } from '../../../data';
 const UserChallenge = () => {
+  const [name, setName] = useState('');
+  const [people, setPeople] = useState(data);
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    setPeople([...people, { name: name, id: Math.random() + Math.random() }]);
+  };
+
+  const handleRemove = (id) => {
+    let newPeople = people.filter((person) => {
+      if (person.id !== id) {
+        return person.name;
+      }
+    });
+    setPeople(newPeople);
+
+    // setPeople(() => {
+    //   return people.map((person) => person.id !== id);
+    // });
+  };
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={onSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            value={name}
+            name="name"
+            id="name"
+            autoComplete="name"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
-      {/* render users below */}
+
+      {people.map((person) => (
+        <>
+          <h4 key={person.id}>{person.name}</h4>
+          <button onClick={() => handleRemove(person.id)} className="btn">
+            Remove
+          </button>
+        </>
+      ))}
     </div>
   );
 };
