eb82158b42b898a4b2de5a275993474e7dcc56cf
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-04 18:28:08 +0530

    Setup Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Starter from './tutorial/05-leverage-javascript/starter/List';
+import Starter from './tutorial/06-forms/starter/01-controlled-inputs';
 import Final from './tutorial/01-useState/final/01-error-example';
 
 function App() {
   return (
     <div className="container">
-      <h2>Advanced React</h2>
+      <h1 style={{ fontStyle: 'italic' }}>Advanced React</h1>
       <Starter></Starter>
     </div>
   );
