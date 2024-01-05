f0339bfd23ea352fcac43e026ff90531d718b884
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-05 21:09:17 +0530

    Finish Other Inputs

--- src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -1,20 +1,45 @@
 import { useState } from 'react';
 const frameworks = ['react', 'angular', 'vue', 'svelte'];
 const OtherInputs = () => {
+  const [shipping, setShipping] = useState(false);
+  const [frame, setFrame] = useState('react');
+  const handleShipping = (e) => {
+    setShipping(e.target.checked);
+  };
+  const handleFrame = (e) => {
+    setFrame(e.target.value);
+  };
   return (
     <div>
-      <form className='form'>
+      <form className="form">
         <h4>Other Inputs</h4>
         {/* name */}
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='shipping'> Free Shipping </label>
+        <div className="form-row" style={{ textAlign: 'left' }}>
+          <label htmlFor="shipping"> Free Shipping </label>
+          <input
+            type="checkbox"
+            name="shipping"
+            id="shipping"
+            checked={shipping}
+            onChange={handleShipping}
+          />
         </div>
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='framework' className='form-label'>
+        <div className="form-row" style={{ textAlign: 'left' }}>
+          <label htmlFor="framework" className="form-label">
             Framework
           </label>
+          <select
+            name="framework"
+            id="framework"
+            value={frame}
+            onChange={handleFrame}
+          >
+            {frameworks.map((framework) => {
+              return <option key={framework}>{framework}</option>;
+            })}
+          </select>
         </div>
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
