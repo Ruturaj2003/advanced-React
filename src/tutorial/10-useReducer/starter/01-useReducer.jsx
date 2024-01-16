6b9720c8653ad8e60979d96a1dbbcef014786f25
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-16 19:20:03 +0530

    Complete Challenge

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -12,19 +12,32 @@ const ReducerBasics = () => {
       {people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
+
+      {people.length > 0 ? (
+        <button
+          className="btn"
+          style={{ marginTop: '2rem' }}
+          onClick={() => setPeople([])}
+        >
+          clear items
+        </button>
+      ) : (
+        <button
+          className="btn"
+          style={{ marginTop: '2rem' }}
+          onClick={() => setPeople(data)}
+        >
+          Reset Data
+        </button>
+      )}
+
+      <br />
     </div>
   );
 };
