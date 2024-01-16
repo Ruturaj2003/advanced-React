71454ab3b14232e0f287051e2cc0909adf8f103c
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-16 21:13:04 +0530

    Implement all functions using own method

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import { data } from '../../../data';
+
+const defaultState = {
+  people: data,
+};
+
+const reducer = (state, action) => {
+  if (action.type === 'CLEAR_LIST') {
+    return {
+      people: [],
+    };
+  }
+
+  if (action.type === 'REMOVE_ITEM') {
+    return {
+      people: action.newPeople,
+    };
+  }
+
+  if (action.type === 'RESET_DATA') {
+    return {
+      people: data,
+    };
+  }
+};
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  // const [people, setPeople] = useState(data);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    let newPeople = state.people.filter((person) => person.id !== id);
+
+    dispatch({
+      type: 'REMOVE_ITEM',
+      newPeople,
+    });
+    // setPeople(newPeople);
+  };
+
+  const clearList = () => {
+    dispatch({
+      type: 'CLEAR_LIST',
+    });
+  };
+
+  const resetData = () => {
+    dispatch({
+      type: 'RESET_DATA',
+    });
   };
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className="item">
@@ -19,11 +63,11 @@ const ReducerBasics = () => {
         );
       })}
 
-      {people.length > 0 ? (
+      {state.people.length > 0 ? (
         <button
           className="btn"
           style={{ marginTop: '2rem' }}
-          onClick={() => setPeople([])}
+          onClick={clearList}
         >
           clear items
         </button>
@@ -31,7 +75,7 @@ const ReducerBasics = () => {
         <button
           className="btn"
           style={{ marginTop: '2rem' }}
-          onClick={() => setPeople(data)}
+          onClick={resetData}
         >
           Reset Data
         </button>
